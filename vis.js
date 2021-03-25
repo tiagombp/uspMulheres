308c23c4d3e511cc1dd40392c1a0c26e9dcf0163
tiagombp <tiagombp@gmail.com>
2021-03-24 22:36:32 -0300

    estrutura inicial js

---
+++ vis.js
@@ -0,0 +1,45 @@
+const vis = {
+
+    refs : {
+
+        svg : "svg.vis",
+        container_svg : "div.container-svg"
+
+    },
+
+    sels : {
+
+        svg : null,
+        container_svg : null,
+        rects : null
+
+    },
+
+    params : {
+
+        dots : {
+
+            qde_fileira : 4,
+            largura : 2,
+            altura : 2
+
+        }
+
+    },
+
+    dims : {
+
+        width : null,
+        height : null
+
+    },
+
+    utils : {},
+
+    render : {},
+
+    control : {}
+
+
+
+}
