d0fda0bbc8e819cfecac9dbecd9f790a8a3bb00d
tiagombp <tiagombp@gmail.com>
2022-03-06 18:55:04 -0300

    processa nova base, corrige ordem de subperguntas, e corrige perguntas que não estavam aparecendo.

--- R/preparacao3.R
+++ R/preparacao3.R
@@ -3,7 +3,7 @@ library(tidyverse)
 library(jsonlite)
 library(weights)
 
-base <- readRDS("./R/base_com_pesos_em_04_02_2022.rds") #readRDS("./R/base_em_20_05_2021.rds")
+base <- readRDS("./R/base_com_pesos_em_06_03_2022.rds") #readRDS("./R/base_em_20_05_2021.rds")
 load("./R/questoes.rda")
 
 
@@ -218,6 +218,226 @@ filtros <- list(
 )
 
 
+# reordena ----------------------------------------------------------------
+
+dput(names(output$trabalho_estudo$G3Q00019$dados))
+
+output$trabalho_estudo$G3Q00019$dados <- 
+  output$trabalho_estudo$G3Q00019$dados[c(
+    "Rotina de ensino a distância", 
+    "Rotina de teletrabalho",
+    "Uso de programas para exibição/apresentação (ex.: Powerpoint etc.)", 
+    "Uso de ferramentas de videochamada"
+  )]
+
+dput(names(output$trabalho_estudo$G3Q00020$dados))
+
+output$trabalho_estudo$G3Q00020$dados <- 
+  output$trabalho_estudo$G3Q00020$dados[c(
+    "Rotina de ensino a distância", 
+    "Rotina de teletrabalho",
+    "Uso de programas para exibição/apresentação (ex.: Powerpoint etc.)", 
+    "Uso de ferramentas de videochamada"
+  )]
+
+dput(names(output$trabalho_estudo$G3Q00021$dados))
+
+output$trabalho_estudo$G3Q00021$dados <- 
+  output$trabalho_estudo$G3Q00021$dados[c(
+    "Programas de videochamada", 
+    "Organização da rotina de teletrabalho",
+    "Programas para exibição/apresentação (ex.: Powerpoint etc.)" 
+    
+  )]
+ 
+dput(names(output$trabalho_estudo$G3Q00022$dados))   
+
+output$trabalho_estudo$G3Q00022$dados <- 
+  output$trabalho_estudo$G3Q00022$dados[c(
+    "Preparação e oferecimento de aulas", 
+    "Participação em eventos acadêmicos (incluindo organização)",
+    "Redação de artigos e patentes",
+    "Realização de novas pesquisas", 
+    "Condução de pesquisas em andamento",
+    "Reuniões administrativas",
+    "Encontros de orientação", 
+    "Aplicação de avaliações"
+ )]
+
+
+dput(names(output$trabalho_estudo$G3Q00024$dados))   
+
+output$trabalho_estudo$G3Q00024$dados <- 
+  output$trabalho_estudo$G3Q00024$dados[c(
+    "Interação com fornecedores",
+    "Interação com usuários", 
+    "Interação com a equipe", 
+    "Interação com outros órgãos da USP"
+  )]
+
+
+dput(names(output$trabalho_estudo$G3Q00023$dados))   
+
+output$trabalho_estudo$G3Q00023$dados <- 
+  output$trabalho_estudo$G3Q00023$dados[c(
+    "Estudo diário", 
+    "Participação em eventos acadêmicos (incluindo organização)",
+    "Redação de artigos e patentes", 
+    "Realização de novas pesquisas", 
+    "Condução de pesquisas em andamento", 
+    "Encontros com o(a) professor(a) extra-classe para atividades acadêmicas",
+    "Realização de avaliações", 
+    "Assistir aulas"
+  )]
+
+
+dput(names(output$trabalho_estudo$G3Q00036$dados))   
+
+output$trabalho_estudo$G3Q00036$dados <- 
+  output$trabalho_estudo$G3Q00036$dados[c(
+    "Luvas de borracha de cano longo", 
+    "Botas impermeáveis",
+    "Placas de acrílico",
+    "\"lavatório/pia com dispensador de sabonete líquido , suporte para papel toalha, papel toalha, lixeira com tampa e abertura sem contato manual\"", 
+    "Máscara de tecido", 
+    "Óculos ou protetor facial", 
+    "Máscara cirúrgica", 
+    "Máscara tipo N95, N99, N100, PFF2 ou PFF3", 
+    "Gorro descartável", 
+    "Avental impermeável de mangas compridas", 
+    "Luvas de procedimento",
+    "Dispensador de preparação alcoólica a 70%"
+  )]
+
+names(output$trabalho_estudo$G3Q00036$dados) <- c(
+  "Luvas de borracha de cano longo", 
+  "Botas impermeáveis",
+  "Placas de acrílico",
+  "lavatório/pia com dispensador de sabonete líquido , suporte para papel toalha, papel toalha, lixeira com tampa e abertura sem contato manual", 
+  "Máscara de tecido", 
+  "Óculos ou protetor facial", 
+  "Máscara cirúrgica", 
+  "Máscara tipo N95, N99, N100, PFF2 ou PFF3", 
+  "Gorro descartável", 
+  "Avental impermeável de mangas compridas", 
+  "Luvas de procedimento",
+  "Dispensador de preparação alcoólica a 70%"
+)
+
+dput(names(output$trabalho_estudo$G3Q00039$dados))   
+
+output$trabalho_estudo$G3Q00039$dados <- 
+  output$trabalho_estudo$G3Q00039$dados[c(
+    "Realização de afazeres domésticos?", 
+    "Cuidado de pessoa(s) com deficiência sob a sua responsabilidade?",
+    "Cuidado de pessoa(s) mais velha(s) sob a sua responsabilidade?",
+    "Cuidado de criança(s) e/ou adolescente(s) sob a sua responsabilidade?"
+  )]
+
+
+dput(names(output$trabalho_estudo$G3Q00041$dados))   
+
+output$trabalho_estudo$G3Q00041$dados <- 
+  output$trabalho_estudo$G3Q00041$dados[c(
+    "Realização de afazeres domésticos?", 
+    "Cuidado de criança(s) e/ou adolescente(s) sob a sua responsabilidade?", 
+    "Cuidado de pessoa(s) mais velha(s) sob a sua responsabilidade?", 
+    "Cuidado de pessoa(s) com deficiência sob a sua responsabilidade?"
+  )]
+
+
+dput(names(output$trabalho_estudo$G3Q00044$dados))   
+
+output$trabalho_estudo$G3Q00044$dados <- 
+  output$trabalho_estudo$G3Q00044$dados[c(
+    "Criança/ adolescente 1",
+    "Criança/ adolescente 2", 
+    "Criança/ adolescente 3", 
+    "Criança/ adolescente 4"
+  )]
+
+
+dput(names(output$trabalho_estudo$G3Q00045$dados))   
+
+output$trabalho_estudo$G3Q00045$dados <- 
+  output$trabalho_estudo$G3Q00045$dados[c(
+    "Criança/ adolescente 1",
+    "Criança/ adolescente 2", 
+    "Criança/ adolescente 3", 
+    "Criança/ adolescente 4" 
+  )]
+
+
+
+dput(names(output$trabalho_estudo$G3Q00046$dados))   
+
+output$trabalho_estudo$G3Q00046$dados <- 
+  output$trabalho_estudo$G3Q00046$dados[c(
+    "Criança/ adolescente 1",
+    "Criança/ adolescente 2", 
+    "Criança/ adolescente 3", 
+    "Criança/ adolescente 4" 
+  )]
+
+
+
+dput(names(output$trabalho_estudo$G3Q00047$dados))   
+
+output$trabalho_estudo$G3Q00047$dados <- 
+  output$trabalho_estudo$G3Q00047$dados[c(
+    "Criança/ adolescente 1",
+    "Criança/ adolescente 2", 
+    "Criança/ adolescente 3", 
+    "Criança/ adolescente 4" 
+  )]
+
+
+dput(names(output$trabalho_estudo$G3Q00048$dados))   
+
+output$trabalho_estudo$G3Q00048$dados <- 
+  output$trabalho_estudo$G3Q00048$dados[c(
+    "Criança/ adolescente 1",
+    "Criança/ adolescente 2", 
+    "Criança/ adolescente 3", 
+    "Criança/ adolescente 4" 
+  )]
+
+
+
+dput(names(output$renda$G04Q239$dados))   
+
+output$renda$G04Q239$dados <- 
+  output$renda$G04Q239$dados[c(
+    "Utilidades domésticas e ferramentas",
+    "Vestuário",
+    "Tecnologia e informática",
+    "Livros e filmes", 
+    "Produtos do supermercado", 
+    "Comida"
+    )]
+
+
+dput(names(output$interacoes_lar$G6Q00019$dados))   
+
+output$interacoes_lar$G6Q00019$dados <- 
+  output$interacoes_lar$G6Q00019$dados[c(
+    "Machucou com um soco ou com algum objeto?",
+    "Empurrou ou deu-lhe um tranco/chacoalhão?", 
+    "Deu-lhe um chute, arrastou ou surrou você?", 
+    "Deu-lhe um tapa ou jogou algo em você que poderia ferir?",
+    "Ameaçou usar ou realmente usou arma de fogo, faca ou outro tipo de arma contra você?",
+    "Estrangulou ou queimou você de propósito?"
+  )]
+
+
+dput(names(output$interacoes_lar$G6Q00021$dados))   
+
+output$interacoes_lar$G6Q00021$dados <- 
+  output$interacoes_lar$G6Q00021$dados[c(
+    "Você teve relação sexual porque estava com medo do que esta pessoa pudesse fazer?",
+    "Forçou fisicamente a manter relações sexuais quando você não queria?", 
+    "Forçou-lhe a uma prática sexual degradante ou humilhante?"
+  )]
 
 # export ------------------------------------------------------------------
 
